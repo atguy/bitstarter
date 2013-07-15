df8d94845131d2bdb69df719aebb3a10caccff83
Guy Malachi <guymal@gmail.com>
2013-07-15 14:27:34 +0000

    read index.html

--- web.js
+++ web.js
@@ -2,8 +2,23 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+/*
+var fs=require('fs');
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+respond.send(data.toString());
+});
+*/
+
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
+var fs=require('fs');
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+respose.send(data.toString());
+});
 });
 
 var port = process.env.PORT || 5000;
